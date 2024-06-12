0a43625c2889fc8c689002c21c75245c8b4ed810
justine <justine.bels04@gmail.com>
2024-06-12 11:33:32 +0200

    initialize

---
+++ src/main/java/com/example/tptdd/TpTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.tptdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tptdd/TpTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tptdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
